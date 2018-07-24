697449d6a59ed06a4c1ac89154949c21f2637eba
safordog <40581256+safordog@users.noreply.github.com>
2018-07-25 02:48:13 +0300

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -2,7 +2,7 @@ package com.gmail.safordog;
 
 public abstract class Shape {
 	
-	abstract double getPerimetr();
+    abstract double getPerimetr();
     abstract double getArea();
     abstract String getType();
 
